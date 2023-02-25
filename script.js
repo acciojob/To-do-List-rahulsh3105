535f7389015623d5c186e0dc4b539b620b15181f
Rahul Sharma <124663428+rahulsh3105@users.noreply.github.com>
2023-02-25 11:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
+
+addTodoBtn.addEventListener("click", () => {
+  const newTodoText = newTodoInput.value.trim();
+  if (newTodoText) {
+    const newTodoItem = document.createElement("li");
+    newTodoItem.textContent = newTodoText;
+    todoList.appendChild(newTodoItem);
+    newTodoInput.value = "";
+  }
+});
+
+newTodoInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTodoBtn.click();
+  }
+});
+
 
